22ba1130730a14f9fac487d7158bff5c779beab0
Qhawe Matimela <67066977+Kingston-5@users.noreply.github.com>
2023-04-17 12:44:40 +0200

    Initial commit

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * @category controllers
+ * @author kingston-5 <qhawe@kingston-enterprises.net>
+ * @license For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace kingstonenterprises\app\controllers;
+
+use kingston\icarus\Application;
+use kingston\icarus\Controller;
+use kingston\icarus\Request;
+
+use kingstonenterprises\app\models\User;
+
+/**
+ * controls the the sites user authorisation functions e.g login, registration and logout
+ *
+ * @extends \kingston\icarus\Controller
+ */
+class AuthController extends Controller
+{
+    /**
+     * render user login page Or if user submitted login form, check 
+     * if user details are valid and login user
+     *
+     * @param Request $request
+     * @return string
+     */
+    public function login(Request $request) : string
+    {
+        $user = new User();
+        
+        if ($request->getMethod() === 'post') {
+            $user->loadData($request->getBody());
+            
+            if ($user->loginValid()) {
+            	$user = $user->findOne(['email' => $request->getBody()['email']]); 
+
+            	Application::$app->session->set('user', $user->id);
+                Application::$app->session->setFlash('success', 'Welcome ' . $user->getDisplayName());
+                Application::$app->response->redirect('/dashboard');
+
+            }
+        }
+        
+        return $this->render('auth/login', [
+        	'title' => 'Login',
+            'model' => $user
+        ]);
+        
+    }
+
+    /**
+     * render user login page Or if user submitted login form, check 
+     * if user details are valid and save them in the Database
+     *
+     * @param Request $request
+     * @return string
+     */
+    public function register(Request $request) : string
+    {
+    	
+        $registerModel = new User();
+        if ($request->getMethod() === 'post') {
+            $registerModel->loadData($request->getBody());
+            if ($registerModel->validate() && $registerModel->save()) {
+                Application::$app->session->setFlash('success', 'Thanks for registering');
+                Application::$app->response->redirect('/auth/login');
+                return 'Show success page';
+            }
+
+        }
+        
+        return $this->render('auth/register', [
+        	'title' => 'Register',
+            'model' => $registerModel
+        ]);
+    }
+
+    /**
+     * logout User
+     *
+     * @return void
+     */
+    public function logout() : void
+    {
+        Application::$app->session->remove('user');
+        Application::$app->response->redirect('/');
+        
+    }
+	
+
+
+}
